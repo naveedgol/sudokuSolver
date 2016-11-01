288f9fcef60342ceff1c6e83a250cf5a21dc8874
Naveed Golafshani <ngolafshani@live.ca>
2016-11-01 18:46:52 -0400

    Added function to count blanks in table, created basic solver function

--- main.cpp
+++ main.cpp
@@ -6,23 +6,39 @@ void printTable(int [9][9]);
 bool rowConflict(int [9][9], int, int);
 bool columnConflict(int [9][9], int, int);
 bool boxConflict(int table[9][9], int column, int row, int number);
-void tableSolver(int table[9][9]);
+void solveTable(int table[9][9]);
+int blankCounter(int table[9][9]);
 
 
 int main() {
-    
+    /*
+     int table[9][9]=
+     {
+     {1,2,3,4,5,6,7,8,9},
+     {4,5,6,7,8,9,1,2,3},
+     {7,8,9,1,2,3,4,5,6},
+     {2,3,4,5,6,7,8,9,1},
+     {5,6,7,8,9,1,2,3,4},
+     {8,9,1,2,3,4,5,6,7},
+     {3,4,5,6,7,8,9,1,2},
+     {6,7,8,9,1,2,3,4,5},
+     {9,1,2,3,4,5,6,7,8}
+     };*/
     int table[9][9]=
     {
-        {0,1,2,3,4,5,6,7,8},
-        {0,1,2,3,4,5,6,7,8},
-        {0,1,2,3,4,5,6,7,8},
-        {0,1,2,3,4,5,6,7,8},
-        {0,1,2,3,4,5,6,7,8},
-        {0,1,2,3,4,5,6,7,8},
-        {0,1,2,3,4,5,6,7,8},
-        {0,1,2,3,4,5,6,7,8},
-        {0,1,2,3,4,5,6,7,8}
+        {0,2,3,4,5,6,0,8,9},
+        {4,0,6,7,8,9,0,2,3},
+        {7,8,0,1,2,3,0,5,0},
+        {2,3,4,0,6,7,0,9,0},
+        {5,6,7,8,0,1,0,3,0},
+        {8,9,1,2,3,0,0,0,0},
+        {3,4,5,6,7,8,0,0,0},
+        {6,7,8,9,1,2,0,0,0},
+        {9,1,2,3,4,5,0,0,0}
     };
+    printTable(table);
+    solveTable(table);
+    printTable(table);
     return 0;
 }
 
@@ -40,6 +56,7 @@ void printTable(int table[9][9])
         if(r==2 or r==5)
             cout<<" -------- --------- --------"<<endl;
     }
+    cout<<endl;
 }
 
 bool rowConflict(int table[9][9], int row, int number)
@@ -75,4 +92,58 @@ bool boxConflict(int table[9][9], int column, int row, int number)
         }
     }
     return true;
+}
+
+int blankCounter(int table[9][9])
+{
+    int blanks=0;
+    for(int r=0;r<9;r++)
+        for(int c=0;c<9;c++)
+            if(table[r][c]==0)
+                blanks++;
+    return blanks;
+}
+
+void solveTable(int table[9][9])
+{
+    bool ineffectiveCycle=false;
+    int blanks=blankCounter(table);
+    while(blanks>0)
+    {
+        if(ineffectiveCycle)
+        {
+            cout<<"Simple solve impossible"<<endl;
+            return;
+        }
+        for(int r=0;r<9;r++)
+        {
+            for(int c=0;c<9;c++)
+            {
+                if(table[r][c]==0)
+                {
+                    int possibilities=0;
+                    for(int n=1;n<=9;n++)
+                    {
+                        if(rowConflict(table,r,n) and columnConflict(table, c, n) and boxConflict(table, c, r, n))
+                        {
+                            if(possibilities==0)
+                            {
+                                table[r][c]=n;
+                                possibilities++;
+                                blanks--;
+                                ineffectiveCycle=false;
+                            }
+                            else
+                            {
+                                table[r][c]=0;
+                                blanks++;
+                                ineffectiveCycle=true;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
