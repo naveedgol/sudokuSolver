b7cd936516fe1aef466897ffa8375206e1b43154
Naveed Golafshani <ngolafshani@live.ca>
2016-11-01 13:54:02 -0400

    added box conflict checking fuction

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@ using namespace std;
 void printTable(int [9][9]);
 bool rowConflict(int [9][9], int, int);
 bool columnConflict(int [9][9], int, int);
+bool boxConflict(int table[9][9], int column, int row, int number);
+void tableSolver(int table[9][9]);
+
 
 int main() {
     
@@ -59,4 +62,17 @@ bool columnConflict(int table[9][9], int column, int number)
     return true;
 }
 
-
+bool boxConflict(int table[9][9], int column, int row, int number)
+{
+    int boxRow=(row)/3; //0,1,2 -> 0; 3,4,5 -> 1; 6,7,8 ->2;
+    int boxColumn=(column)/3;
+    for(int r=boxRow*3;r<boxRow*3+3;r++)
+    {
+        for(int c=boxColumn*3;c<boxColumn*3+3;c++)
+        {
+            if(table[r][c]==number)
+                return false;
+        }
+    }
+    return true;
+}
