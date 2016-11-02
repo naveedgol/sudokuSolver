8e14b51de0eec630a6e8dda7f3462c4bdcdcf6ae
Naveed Golafshani <ngolafshani@live.ca>
2016-11-02 11:50:33 -0400

    simplified conflict checks, improved readibility

--- main.cpp
+++ main.cpp
@@ -3,39 +3,47 @@
 using namespace std;
 
 void printTable(int [9][9]);
-bool rowConflict(int [9][9], int, int);
-bool columnConflict(int [9][9], int, int);
-bool boxConflict(int table[9][9], int column, int row, int number);
-void solveTable(int table[9][9]);
-int blankCounter(int table[9][9]);
+bool isRowConflicting(int [9][9], int, int);
+bool isColumnConflicting(int [9][9], int, int);
+bool isBoxConflicting(int [9][9], int, int, int);
+void solveTable(int [9][9]);
+int blankCounter(int [9][9]);
+bool isValid(int [9][9], int, int, int);
 
 
 int main() {
+    
     /*
      int table[9][9]=
      {
-     {1,2,3,4,5,6,7,8,9},
-     {4,5,6,7,8,9,1,2,3},
-     {7,8,9,1,2,3,4,5,6},
-     {2,3,4,5,6,7,8,9,1},
-     {5,6,7,8,9,1,2,3,4},
-     {8,9,1,2,3,4,5,6,7},
-     {3,4,5,6,7,8,9,1,2},
-     {6,7,8,9,1,2,3,4,5},
-     {9,1,2,3,4,5,6,7,8}
+     {1,2,3, 4,5,6, 7,8,9},
+     {4,5,6, 7,8,9, 1,2,3},
+     {7,8,9, 1,2,3, 4,5,6},
+     
+     {2,3,4, 5,6,7, 8,9,1},
+     {5,6,7, 8,9,1, 2,3,4},
+     {8,9,1, 2,3,4, 5,6,7},
+     
+     {3,4,5, 6,7,8, 9,1,2},
+     {6,7,8, 9,1,2, 3,4,5},
+     {9,1,2, 3,4,5, 6,7,8}
      };*/
+    
     int table[9][9]=
     {
-        {0,2,3,4,5,6,0,8,9},
-        {4,0,6,7,8,9,0,2,3},
-        {7,8,0,1,2,3,0,5,0},
-        {2,3,4,0,6,7,0,9,0},
-        {5,6,7,8,0,1,0,3,0},
-        {8,9,1,2,3,0,0,0,0},
-        {3,4,5,6,7,8,0,0,0},
-        {6,7,8,9,1,2,0,0,0},
-        {9,1,2,3,4,5,0,0,0}
+        {0,2,3, 4,5,6, 0,8,9},
+        {4,0,6, 7,8,9, 0,2,3},
+        {7,8,0, 1,2,3, 0,5,0},
+        
+        {2,3,4, 0,6,7, 0,9,0},
+        {5,6,7, 8,0,1, 0,3,0},
+        {8,9,1, 2,3,0, 0,0,0},
+        
+        {3,4,5, 6,7,8, 0,0,0},
+        {6,7,8, 9,1,2, 0,0,0},
+        {9,1,2, 3,4,5, 0,0,0}
     };
+    
     printTable(table);
     solveTable(table);
     printTable(table);
@@ -59,27 +67,27 @@ void printTable(int table[9][9])
     cout<<endl;
 }
 
-bool rowConflict(int table[9][9], int row, int number)
+bool isRowConflicting(int table[9][9], int row, int number)
 {
     for(int c=0;c<9;c++)
     {
         if(table[row][c]==number)
-            return false;
+            return true;
     }
-    return true;
+    return false;
 }
 
-bool columnConflict(int table[9][9], int column, int number)
+bool isColumnConflicting(int table[9][9], int column, int number)
 {
     for(int r=0;r<9;r++)
     {
         if(table[r][column]==number)
-            return false;
+            return true;
     }
-    return true;
+    return false;
 }
 
-bool boxConflict(int table[9][9], int column, int row, int number)
+bool isBoxConflicting(int table[9][9], int column, int row, int number)
 {
     int boxRow=(row)/3; //0,1,2 -> 0; 3,4,5 -> 1; 6,7,8 ->2;
     int boxColumn=(column)/3;
@@ -88,9 +96,16 @@ bool boxConflict(int table[9][9], int column, int row, int number)
         for(int c=boxColumn*3;c<boxColumn*3+3;c++)
         {
             if(table[r][c]==number)
-                return false;
+                return true;
         }
     }
+    return false;
+}
+
+bool isValid(int table[9][9], int column, int row, int number)
+{
+    if(isRowConflicting(table, row, number) || isColumnConflicting(table, column, number) || isBoxConflicting(table, column, row, number))
+        return false;
     return true;
 }
 
@@ -124,7 +139,7 @@ void solveTable(int table[9][9])
                     int possibilities=0;
                     for(int n=1;n<=9;n++)
                     {
-                        if(rowConflict(table,r,n) and columnConflict(table, c, n) and boxConflict(table, c, r, n))
+                        if(isValid(table,c,r,n))
                         {
                             if(possibilities==0)
                             {
